36d0a96b560cfd234b05da24673ac9ee05160551
Jules Fayol <jules.fayol@epfprojets.com>
2023-11-21 02:06:14 +0100

    test reussi

--- arduinoCodeFolder/src/main.cpp
+++ arduinoCodeFolder/src/main.cpp
@@ -1,11 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+#include <time.h>
 
 const char* ssid = "Freebox-16D4DD";
 const char* password = "inperabit&-vicinalem-flabrali6-disruperit";
 
 const char* host = "louetonfilm.com";
-const int httpsPort = 443; // Port 443 for HTTPS
+const int httpsPort = 443; // Port 443 pour HTTPS
 
 WiFiClientSecure client;
 
@@ -18,40 +19,58 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 
-  client.setInsecure(); // Bypass SSL certificate verification
+  // Configurer la mise à jour de l'heure via NTP
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
 
-  if (!client.connect(host, httpsPort)) {
-    Serial.println("Connection failed");
-    return;
+  // Attendre que l'heure soit mise à jour
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
   }
 
-  // Compose a GET request with an example action 'testAction'
-  String url = "/api.php?action=testAction";
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
+  Serial.println("\nTime updated");
 
-  // Wait for the response and read it
-    while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      break; // Les en-têtes sont terminés, le corps commence
-    }
-  }
-
-  // Lisez le corps de la réponse qui est au format JSON
-  String response = client.readString();
-  Serial.println("Received response:");
-  Serial.println(response);
-
+  // Désactiver la vérification du certificat SSL (non recommandé pour la production)
+  client.setInsecure();
 }
 
 void loop() {
-  // No need to repeat the action in the loop.
-}
+  // Récupérer l'heure actuelle
+  time_t now = time(nullptr);
+  struct tm* p_tm = localtime(&now);
 
+  // Convertir l'heure en une chaîne de caractères
+  char timeString[16];
+  strftime(timeString, sizeof(timeString), "%H:%M:%S", p_tm);
+  
+  // Créer une requête HTTP avec l'heure
+  if (client.connect(host, httpsPort)) {
+    String url = String("/api.php?time=") + timeString;
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" + 
+                 "Connection: close\r\n\r\n");
+
+    // Attendre que le serveur réponde
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        break; // Fin des en-têtes HTTP
+      }
+    }
+
+    // Lire le corps de la réponse
+    while (client.available()) {
+      String line = client.readStringUntil('\n');
+      Serial.println(line);
+    }
+
+    client.stop(); // Fermer la connexion
+  } else {
+    Serial.println("Connection failed");
+  }
+
+  // Attendre une minute avant d'envoyer la prochaine requête
+  delay(60000);
+}
